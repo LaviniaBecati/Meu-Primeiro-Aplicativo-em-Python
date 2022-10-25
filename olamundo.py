98a10ba2b34375da5a234032f0a5960477da9522
LaviniaBecati <116568473+LaviniaBecati@users.noreply.github.com>
2022-10-25 11:19:19 -0300

    Criado a app imc
    Iniciado desenvolvimento do algoritmo de IMC
    adicionado entrada e saída de dados

---
+++ olamundo.py
@@ -0,0 +1,12 @@
+# coletando dados do usuario
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+
+print("-" * 30)
+print("OS DADOS COLETADOS FORAM: ")
+print("NOME: ",nome)
+print("IDADE: ",idade," anos")
+print("ALTURA: ",altura)
+print("PESO: ",peso," kgs")
