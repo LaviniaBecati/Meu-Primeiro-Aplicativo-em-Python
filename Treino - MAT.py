2a58ed3c0508506d749feda50eaebb1da14dc61a
LaviniaBecati <116568473+LaviniaBecati@users.noreply.github.com>
2022-10-25 11:53:35 -0300

    Adicionado calculadora IMC
    Adicionando calculadora em formula imc

--- Treino - MAT.py
+++ Treino - MAT.py
@@ -1,9 +1,12 @@
 n1= float(input("Digite o primeiro número: "))
 n2= float(input("Digite o segundo número: "))
-
+imc= peso/altura**2
 print("Soma =", n1 + n2)
 print("Subtração =", n1- n2)
 print("Multiplicação =", n1 * n2)
 print("Divisão =", n1 / n2)
 print("Quadrado do 1=", n1**2)
 print("Quadrado do 2 =", n2**2)
+
+
+
--- olamundo.py
+++ olamundo.py
@@ -3,10 +3,11 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
-
+imc= peso/altura**2
 print("-" * 30)
 print("OS DADOS COLETADOS FORAM: ")
 print("NOME: ",nome)
 print("IDADE: ",idade," anos")
 print("ALTURA: ",altura)
 print("PESO: ",peso," kgs")
+print("IMC = ", imc)
